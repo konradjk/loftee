6a99f18ae1c69a2306e2fc46f1586653b301ab8c
Daniel Birnbaum <birndle@users.noreply.github.com>
2016-11-28 22:31:06 -0500

    Update splice_module.pl

--- splice_module.pl
+++ splice_module.pl
@@ -3,7 +3,7 @@ use strict;
 sub check_5UTR_splice {
     my $transcript_variation = shift;
     my $variation_feature = shift;
-    my $strand = $variation_feature->{strand};
+    my $strand = $transcript_variation->transcript->strand;
 
     # Start is always less than or equal to end regardless of the orientation of the slice (true for introns as well).
     my $slice = $variation_feature->feature_Slice();
@@ -21,7 +21,7 @@ sub check_5UTR_splice {
 sub check_3UTR_splice {
     my $transcript_variation = shift;
     my $variation_feature = shift;
-    my $strand = $variation_feature->{strand};
+    my $strand = $transcript_variation->transcript->strand;
 
     # Start is always less than or equal to end regardless of the orientation of the slice (true for introns as well).
     my $slice = $variation_feature->feature_Slice();
